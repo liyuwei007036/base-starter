503f03b1a92fafa8f4c6901140ce49c987c545db
liyuwi <dddd>
2019-11-22 23:18:00 +0800

    代码优化

--- src/main/java/com/lc/core/config/ThreadPoolConfig.java
+++ src/main/java/com/lc/core/config/ThreadPoolConfig.java
@@ -1,14 +1,12 @@
 package com.lc.core.config;
 
-import com.lc.core.config.properties.TheadPoolProperties;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.stereotype.Component;
 
 import java.util.concurrent.Executor;
 import java.util.concurrent.ThreadPoolExecutor;
@@ -19,26 +17,51 @@ import java.util.concurrent.ThreadPoolExecutor;
  */
 @Slf4j
 @EnableScheduling
-@EnableConfigurationProperties(TheadPoolProperties.class)
-@Configuration
-public class TheadPoolConfig implements AsyncConfigurer {
+@Component
+public class ThreadPoolConfig implements AsyncConfigurer {
+    /**
+     * 核心线程数
+     */
+    @Value("${pool.core-size:80}")
+    private Integer poolCoreSize;
 
-    @Autowired
-    private TheadPoolProperties theadPoolProperties;
+    /**
+     * 最大线程数
+     */
+    @Value("${pool.max-size:200}")
+    private Integer poolMaxSize;
+
+    /**
+     * 队列容量
+     */
+    @Value("${pool.queue-capacity:200}")
+    private Integer queueCapacity;
+
+    /**
+     * 线程活跃时间（秒）
+     */
+    @Value("${pool.keep-alive-seconds:200}")
+    private Integer keepAliveSeconds;
+
+    /**
+     * 设置默认线程名称
+     */
+    @Value("${pool.thread-name-prefix:async-thread-}")
+    private String threadNamePrefix;
 
     @Override
     public Executor getAsyncExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         // 设置核心线程数 80
-        executor.setCorePoolSize(theadPoolProperties.getCorePoolSize());
+        executor.setCorePoolSize(poolCoreSize);
         // 设置最大线程数 200
-        executor.setMaxPoolSize(theadPoolProperties.getMaxPoolSize());
+        executor.setMaxPoolSize(poolMaxSize);
         // 设置队列容量 200
-        executor.setQueueCapacity(theadPoolProperties.getQueueCapacity());
+        executor.setQueueCapacity(queueCapacity);
         // 设置线程活跃时间（秒）
-        executor.setKeepAliveSeconds(theadPoolProperties.getKeepAliveSeconds());
+        executor.setKeepAliveSeconds(keepAliveSeconds);
         // 设置默认线程名称
-        executor.setThreadNamePrefix(theadPoolProperties.getThreadNamePrefix());
+        executor.setThreadNamePrefix(threadNamePrefix);
         // 设置拒绝策略
         executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
         // 是否允许核心线程空闲退出，默认值为false。
@@ -51,7 +74,6 @@ public class TheadPoolConfig implements AsyncConfigurer {
     @Override
     public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
         return (throwable, method, objects) -> {
-
             log.info("throwable", throwable);
             log.info("method{}", method.getName());
             log.info("objects", objects);
