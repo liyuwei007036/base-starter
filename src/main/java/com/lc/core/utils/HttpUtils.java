16c9fb77f8e0af780dc0f7091edb1fb2c686ca4c
lc <599047945@qq.com>
2020-01-05 18:31:53 +0800

    修改bug

--- src/main/java/com/lc/core/utils/HttpUtils.java
+++ src/main/java/com/lc/core/utils/HttpUtils.java
@@ -6,16 +6,12 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.ResponseErrorHandler;
 import org.springframework.web.client.RestTemplate;
 
-import javax.servlet.ServletInputStream;
-import javax.servlet.http.HttpServletRequest;
-import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
@@ -55,6 +51,7 @@ public final class HttpUtils {
      */
     public static String post(String url, Map<String, Object> data, Map<String, String> head) {
         restTemplate.setErrorHandler(getResponseErrorHandler());
+        restTemplate.getMessageConverters().set(1,new StringHttpMessageConverter(StandardCharsets.UTF_8));
         HttpHeaders headers = new HttpHeaders();
         head.forEach(headers::add);
         String ct = ObjectUtil.getString(head.get("Content-Type"));
@@ -62,11 +59,9 @@ public final class HttpUtils {
         // 根据不同的请求头发送
         if (MediaType.MULTIPART_FORM_DATA_VALUE.equals(ct) || (MediaType.APPLICATION_FORM_URLENCODED_VALUE.equals(ct))) {
             MultiValueMap<String, Object> postParameters = new LinkedMultiValueMap<>();
-            for (String m : data.keySet()) {
-                postParameters.add(m, data.get(m));
-            }
+            data.forEach(postParameters::add);
             r = new HttpEntity<>(postParameters, headers);
-        } else{
+        } else {
             r = new HttpEntity<>(JSONObject.toJSONString(data), headers);
         }
         return restTemplate.postForEntity(url, r, String.class).getBody();
