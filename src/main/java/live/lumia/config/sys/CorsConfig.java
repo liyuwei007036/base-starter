1c99a3e4693a4d36c1d8fb73ddb4d3c630f67e60
lc <l599047945@live.com>
2021-02-06 16:17:20 +0800

    代码优化

--- src/main/java/live/lumia/config/sys/CorsConfig.java
+++ src/main/java/live/lumia/config/sys/CorsConfig.java
@@ -4,7 +4,6 @@ import live.lumia.config.properties.SessionNameProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -18,15 +17,11 @@ public class CorsConfig {
     private CorsConfiguration buildConfig(SessionNameProperties sessionNameProperties) {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         // 允许任何域名使用
-        corsConfiguration.addAllowedOrigin("http://127.0.0.1:8080");
+        corsConfiguration.addAllowedOrigin("*");
         // 允许任何头
         corsConfiguration.addAllowedHeader("*");
         // 允许任何方法（post、get等）
-        corsConfiguration.addAllowedMethod(HttpMethod.DELETE);
-        corsConfiguration.addAllowedMethod(HttpMethod.PATCH);
-        corsConfiguration.addAllowedMethod(HttpMethod.POST);
-        corsConfiguration.addAllowedMethod(HttpMethod.GET);
-        corsConfiguration.addAllowedMethod(HttpMethod.OPTIONS);
+        corsConfiguration.addAllowedMethod("*");
         corsConfiguration.setMaxAge(3600L);
         corsConfiguration.addExposedHeader(sessionNameProperties.getName());
         return corsConfiguration;
