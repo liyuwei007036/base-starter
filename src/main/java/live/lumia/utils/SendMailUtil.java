1bf802eb8fd6996e6d0791be09164e2b488f24ab
lc <l599047945@live.com>
2021-02-08 08:57:48 +0800

    代码优化

--- src/main/java/live/lumia/utils/SendMailUtil.java
+++ src/main/java/live/lumia/utils/SendMailUtil.java
@@ -1,18 +1,12 @@
-package live.lumia.service;
+package live.lumia.utils;
 
 import live.lumia.enums.BaseErrorEnums;
 import live.lumia.error.BaseException;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.core.io.FileSystemResource;
-import org.springframework.mail.MailMessage;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
 import javax.mail.internet.MimeMessage;
@@ -25,16 +19,12 @@ import java.io.File;
  * @author lc
  */
 @Slf4j
-@ConditionalOnClass(MailMessage.class)
-@ConditionalOnProperty(prefix = "spring.mail", value = "username")
-@Component
-public class MailService {
+public class SendMailUtil {
 
-    @Value("${spring.mail.username}")
-    private String from;
 
-    @Autowired
-    private JavaMailSender mailSender;
+    public static JavaMailSender getMailSender() {
+        return SpringUtil.getBean(JavaMailSender.class);
+    }
 
     /**
      * 发送邮件
@@ -43,13 +33,13 @@ public class MailService {
      * @param subject
      * @param content
      */
-    public void sendSimpleMail(String to, String subject, String content) {
+    public static void sendSimpleMail(String from, String to, String subject, String content) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(to);
         message.setSubject(subject);
         message.setText(content);
         message.setFrom(from);
-        mailSender.send(message);
+        getMailSender().send(message);
     }
 
     /**
@@ -59,15 +49,15 @@ public class MailService {
      * @param subject
      * @param content
      */
-    public void sendHTMLMail(String to, String subject, String content) {
-        MimeMessage message = mailSender.createMimeMessage();
+    public static void sendHtmlMail(String from, String to, String subject, String content) {
+        MimeMessage message = getMailSender().createMimeMessage();
         try {
             MimeMessageHelper helper = new MimeMessageHelper(message, true);
             helper.setFrom(from);
             helper.setTo(to);
             helper.setSubject(subject);
             helper.setText(content, true);
-            mailSender.send(message);
+            getMailSender().send(message);
         } catch (Exception e) {
             log.error("【邮件发送失败】", e);
         }
@@ -81,8 +71,8 @@ public class MailService {
      * @param content
      * @param f
      */
-    public void sendAttachmentMail(String to, String subject, String content, File f) {
-        MimeMessage message = mailSender.createMimeMessage();
+    public static void sendAttachmentMail(String from, String to, String subject, String content, File f) {
+        MimeMessage message = getMailSender().createMimeMessage();
         try {
             MimeMessageHelper helper = new MimeMessageHelper(message, true);
             helper.setFrom(from);
@@ -98,7 +88,7 @@ public class MailService {
             helper.addAttachment(fileName, file);
             //发送多个附件的操纵
             helper.addAttachment(fileName + "_", file);
-            mailSender.send(message);
+            getMailSender().send(message);
         } catch (Exception e) {
             log.error("【邮件发送失败】", e);
         }
@@ -112,9 +102,9 @@ public class MailService {
      * @param rscPath
      * @param rscId
      */
-    public void sendInLiResourceMail(String to, String subject, String content, File rscPath, String rscId) {
+    public static void sendInLiResourceMail(String from, String to, String subject, String content, File rscPath, String rscId) {
         log.info("发送静态邮件开始：{},{},{},{},{}", to, subject, content, rscPath, rscId);
-        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessage message = getMailSender().createMimeMessage();
         try {
             MimeMessageHelper helper = new MimeMessageHelper(message, true);
             helper.setFrom(from);
@@ -123,7 +113,7 @@ public class MailService {
             helper.setText(content, true);
             FileSystemResource res = new FileSystemResource(rscPath);
             helper.addInline(rscId, res);
-            mailSender.send(message);
+            getMailSender().send(message);
             log.info("发送邮件成功");
         } catch (Exception e) {
             log.error("发送邮件失败：", e);
